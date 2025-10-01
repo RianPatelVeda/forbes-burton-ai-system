994358c773f408a403d8f1308758605bbfba9840
RianPatelVeda <rian@vedaai.co.uk>
2025-10-01 01:36:50 +0100

    Create helpers.js

---
+++ src/utils/helpers.js
@@ -0,0 +1,17 @@
+// Utility Helper Functions - Placeholder
+// This file will contain utility helper functions
+
+export const formatDate = (date) => {
+  // TODO: Implement date formatting
+  return date;
+};
+
+export const validateEmail = (email) => {
+  // TODO: Implement email validation
+  return true;
+};
+
+export default {
+  formatDate,
+  validateEmail
+};
