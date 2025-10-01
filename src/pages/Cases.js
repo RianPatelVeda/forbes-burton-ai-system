a5768a18a1d601d28a0faec51a6a80a1f4584fb9
RianPatelVeda <rian@vedaai.co.uk>
2025-10-01 01:30:26 +0100

    Create Cases.js

---
+++ src/pages/Cases.js
@@ -0,0 +1,13 @@
+// Cases Page
+import React from 'react';
+
+const Cases = () => {
+  return (
+    <div>
+      <h1>Cases</h1>
+      <p>Manage your cases here</p>
+    </div>
+  );
+};
+
+export default Cases;
