c4c118b73e45af481f9b9160cbc83347cfe5a087
RianPatelVeda <rian@vedaai.co.uk>
2025-10-01 01:31:31 +0100

    Create Tasks.js

---
+++ src/pages/Tasks.js
@@ -0,0 +1,13 @@
+// Tasks Page
+import React from 'react';
+
+const Tasks = () => {
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <p>Manage your tasks here</p>
+    </div>
+  );
+};
+
+export default Tasks;
