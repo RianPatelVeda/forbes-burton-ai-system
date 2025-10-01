a1479104974d47ef8e27d217c6dc8a6bac898cb0
RianPatelVeda <rian@vedaai.co.uk>
2025-10-01 01:29:57 +0100

    Create Leads.js

---
+++ src/pages/Leads.js
@@ -0,0 +1,13 @@
+// Leads Page
+import React from 'react';
+
+const Leads = () => {
+  return (
+    <div>
+      <h1>Leads</h1>
+      <p>Manage your leads here</p>
+    </div>
+  );
+};
+
+export default Leads;
