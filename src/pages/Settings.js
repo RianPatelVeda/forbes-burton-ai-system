5d4e10ecf300be3ff80ea9eeff59dced75b51b82
RianPatelVeda <rian@vedaai.co.uk>
2025-10-01 01:32:00 +0100

    Create Settings.js

---
+++ src/pages/Settings.js
@@ -0,0 +1,13 @@
+// Settings Page
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>Settings</h1>
+      <p>Configure your application settings</p>
+    </div>
+  );
+};
+
+export default Settings;
