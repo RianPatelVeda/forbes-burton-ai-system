50bf2ff4e01d440832b574a009e3ae84b9cac90c
RianPatelVeda <rian@vedaai.co.uk>
2025-10-01 01:35:52 +0100

    Create zohoApi.js

---
+++ src/services/zohoApi.js
@@ -0,0 +1,22 @@
+// Zoho API Service - Placeholder
+// This file will handle Zoho CRM API integration
+
+export const zohoApi = {
+  // Placeholder functions for Zoho CRM integration
+  getLeads: async () => {
+    // TODO: Implement Zoho CRM leads fetching
+    return [];
+  },
+  
+  getCases: async () => {
+    // TODO: Implement Zoho CRM cases fetching
+    return [];
+  },
+  
+  getTasks: async () => {
+    // TODO: Implement Zoho CRM tasks fetching
+    return [];
+  }
+};
+
+export default zohoApi;
